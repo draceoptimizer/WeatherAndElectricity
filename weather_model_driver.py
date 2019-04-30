94241826f5ca4f586b420f8dc9509681ce5f677c
David Race <dr.david.race@gmail.com>
2019-04-30 15:43:21 -0500

    minor update

--- weather_model_driver.py
+++ weather_model_driver.py
@@ -16,6 +16,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v","--verbose",dest="verbose",
     help="Increase the verbosity (no effect now)",
     action="store_true", default=False )
+parser.add_argument("--gather-weather",dest="gather_weather",
+    help="Increase the verbosity (no effect now)",
+    action="store_true", default=False )
 parms = parser.parse_args()
 
 if __name__ == "__main__":
@@ -27,4 +30,6 @@ if __name__ == "__main__":
 
     if parms.verbose:
         pprint(start_config)
+
+    print(parms.gather_weather)
 
